6591f1681c5697ce4739acff0e1b5c1531a32cc8
Rajiv Sharma <76260754+rajivsharma111@users.noreply.github.com>
2025-04-27 17:30:05 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* ******************* Typing Animation */
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Java Ethusiast",
+    "Web Developer",
+    
+    "Backend Developer",
+    "Software Engineer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
